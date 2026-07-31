f6519098fa38199491b74b58506d363806b88e4d
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-07-31 17:33:45 +0530

    add pe in client productdetail page

--- src/pages/client/ProductDetail.jsx
+++ src/pages/client/ProductDetail.jsx
@@ -156,7 +156,7 @@ export default function ProductDetail() {
             </div>
           )}
 
-          {/* Quantity */}
+          {/* Quantitype */}
           <div className="mb-10">
             <label className="block text-sm font-semibold text-gray-900 mb-4 uppercase tracking-wider">
               Quantity
