e2955dbe5336e78d3722952a7edd862a7a1b0b9f
Jagdish Jagdale <jagdishjagdale09@gmail.com>
2026-08-16 10:45:35 +0530

    remove pu in client login page

--- src/pages/client/Login.jsx
+++ src/pages/client/Login.jsx
@@ -105,7 +105,7 @@ export default function Login() {
             to="/signup"
             className="text-emerald-600 font-semibold hover:underline"
           >
-            Sign Uppu
+            Sign Up
           </Link>
         </p>
       </div>
